243a873a35dfd073577641c5381cf36a8c55ec73
Julián Sánchez <julian.sanchez@501labs.com.ar>
2021-06-13 12:48:58 -0300

    Se agrega clase de vehiculos

---
+++ src/models/vehiculos.model.js
@@ -0,0 +1,10 @@
+class Vehiculos {
+    constructor(id, patente, marca, modelo) {
+        this.id = id;
+        this.patente = patente;
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+}
+
+module.exports = Vehiculos;
